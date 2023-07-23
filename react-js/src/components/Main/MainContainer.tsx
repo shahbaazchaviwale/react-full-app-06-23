e28e1ea52e0a55dea8c6604a2e0e78bff6210dc6
Shahbaaz <shahbaazc@gmail.com>
2023-07-23 18:37:11 +0530

    added header bar

---
+++ react-js/src/components/Main/MainContainer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const MainContainer = () => {
+  return (
+    <>
+      <main>
+        <div className="mx-auto max-w-5xl py-6 sm:px-6 lg:px-8">
+          {/* Your content */}
+          sdasd
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default MainContainer;
